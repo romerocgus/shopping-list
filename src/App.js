d5d70c5d85471d9d35984d3acb9f5a4fb0ced985
Gustavo Ezequiel Romero Canas <romeroc.gus@gmail.com>
2023-02-13 17:44:17 +0100

    splitted in components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React, { useState } from "react";
 
-import {
-  Typography,
-  Box,
-  InputAdornment,
-  FormControl,
-  InputLabel,
-  IconButton,
-  OutlinedInput,
-  Card,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import CheckIcon from "@mui/icons-material/Check";
+import { Typography, Box } from "@mui/material";
 
 import { mockList } from "./Shared/Assets/mockList";
+import { ShoppingList } from "./Components/ShoppingList";
+import { SearchBar } from "./Components/SearchBar";
 
 function App() {
   const [shoppingList, setShoppingList] = useState(mockList);
   const [showAddButton, setShowAddButton] = useState(false);
   const [searchCriteria, setSearchCriteria] = useState("");
 
-  let filteredList = [];
+  let searchedList = [];
   if (searchCriteria.length === 0) {
-    filteredList = shoppingList;
+    searchedList = shoppingList;
   } else {
-    filteredList = shoppingList.filter((elem) =>
+    searchedList = shoppingList.filter((elem) =>
       elem.name.includes(searchCriteria)
     );
   }
@@ -64,79 +52,18 @@ function App() {
   return (
     <Box display="flex" flexDirection="column" gap={4} alignItems="center">
       <Typography variant="h3">Shopping List</Typography>
-      <FormControl variant="outlined" sx={{ minWidth: 300 }}>
-        <InputLabel htmlFor="search">Search</InputLabel>
-        <OutlinedInput
-          id="search"
-          label="Search"
-          type="text"
-          value={searchCriteria}
-          onChange={handleSearchChange}
-          endAdornment={
-            showAddButton && (
-              <InputAdornment position="end">
-                <IconButton aria-label="add element" onClick={handleAddElement}>
-                  {<AddIcon />}
-                </IconButton>
-              </InputAdornment>
-            )
-          }
-        />
-      </FormControl>
-      <Card variant="outlined" sx={{ minWidth: 300, paddingTop: 2 }}>
-        <Box display="flex" flexDirection="column">
-          <Typography variant="subtitle1" align="center" mb={4}>
-            To Buy
-          </Typography>
-          <List>
-            {filteredList &&
-              filteredList
-                .filter((elem) => elem.inStock === false)
-                .map((elem) => (
-                  <ListItem
-                    key={`toBuy-${elem.name}`}
-                    secondaryAction={
-                      <IconButton
-                        aria-label="bought"
-                        onClick={() => handleToggleBuy(elem.name)}
-                      >
-                        <CheckIcon />
-                      </IconButton>
-                    }
-                  >
-                    <ListItemText id={elem.name} primary={elem.name} />
-                  </ListItem>
-                ))}
-          </List>
-        </Box>
-      </Card>
-      <Card variant="outlined" sx={{ minWidth: 300, paddingTop: 2 }}>
-        <Box display="flex" flexDirection="column">
-          <Typography variant="subtitle1" align="center" mb={4}>
-            Already Have
-          </Typography>
-          <List>
-            {filteredList &&
-              filteredList
-                .filter((elem) => elem.inStock === true)
-                .map((elem) => (
-                  <ListItem
-                    key={`have-${elem.name}`}
-                    secondaryAction={
-                      <IconButton
-                        aria-label="add"
-                        onClick={() => handleToggleBuy(elem.name)}
-                      >
-                        <AddIcon />
-                      </IconButton>
-                    }
-                  >
-                    <ListItemText id={elem.name} primary={elem.name} />
-                  </ListItem>
-                ))}
-          </List>
-        </Box>
-      </Card>
+      <SearchBar
+        searchCriteria={searchCriteria}
+        onSearchChange={handleSearchChange}
+        onButtonClick={handleAddElement}
+        showButton={showAddButton}
+      />
+      <ShoppingList
+        elements={searchedList}
+        onToggleBuy={handleToggleBuy}
+        isBuy={true}
+      />
+      <ShoppingList elements={searchedList} onToggleBuy={handleToggleBuy} />
     </Box>
   );
 }
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  InputAdornment,
+  FormControl,
+  InputLabel,
+  IconButton,
+  OutlinedInput,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+const SearchBar = ({
+  searchCriteria,
+  onSearchChange,
+  onButtonClick,
+  showButton,
+}) => {
+  return (
+    <FormControl variant="outlined" sx={{ minWidth: 300 }}>
+      <InputLabel htmlFor="search">Search</InputLabel>
+      <OutlinedInput
+        id="search"
+        label="Search"
+        type="text"
+        value={searchCriteria}
+        onChange={onSearchChange}
+        endAdornment={
+          showButton && (
+            <InputAdornment position="end">
+              <IconButton aria-label="add element" onClick={onButtonClick}>
+                {<AddIcon />}
+              </IconButton>
+            </InputAdornment>
+          )
+        }
+      />
+    </FormControl>
+  );
+};
+
+export { SearchBar };
