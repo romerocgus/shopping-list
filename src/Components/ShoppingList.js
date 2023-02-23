2cba95081301e15f464f94790ce6775855296ec5
Gustavo Ezequiel Romero Canas <romeroc.gus@gmail.com>
2023-02-23 10:43:21 +0100

    first day class, basic ui and togglebuy ft

---
+++ src/Components/ShoppingList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card, Box, Typography, List } from "@mui/material";
+import { ShoppingListItem } from "./ShoppingListItem";
+
+const ShoppingList = ({ elements, onToggleBuy, isBuy }) => {
+  return (
+    <Card variant="outlined" sx={{ minWidth: 300, paddingTop: 2 }}>
+      <Box display="flex" flexDirection="column">
+        <Typography variant="subtitle1" align="center" mb={4}>
+          titulo
+        </Typography>
+        <List>
+          {elements
+            .filter((elem) => elem.inStock === isBuy)
+            .map((elem) => (
+              <ShoppingListItem
+                key={`shopping-item-${elem.name}`}
+                label={elem.name}
+                isBuy={isBuy}
+                onToggleBuy={onToggleBuy}
+              />
+            ))}
+        </List>
+      </Box>
+    </Card>
+  );
+};
+
+export { ShoppingList };
