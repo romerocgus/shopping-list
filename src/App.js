3759dbc93903662539dece799c8289a70a36a29e
crganf <cristinaganfue@gmail.com>
2023-02-23 12:04:41 +0100

    handleToggleBuy feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Typography, Box } from "@mui/material";
+import { SearchBar } from "./Components/SearchBar";
+import { ShoppingList } from "./Components/ShoppingList";
+import { mockList } from "./Shared/Assets/mockList";
 
 function App() {
+  const [items, setItems] = useState(mockList);
+
+  const handleToggleBuy = (label) => {
+    const newList = items.map((elem) => {
+      if (elem.name === label) {
+        elem.inStock = !elem.inStock;
+      }
+      return elem;
+    });
+    setItems(newList);
+  };
+
   return (
-    <>
+    <Box display="flex" flexDirection="column" gap={4} alignItems="center">
       <Typography variant="h3" align="center">
         Shopping List
       </Typography>
-    </>
+      <SearchBar />
+      <ShoppingList elements={items} onToggleBuy={handleToggleBuy} isBuy />
+      <ShoppingList
+        elements={items}
+        onToggleBuy={handleToggleBuy}
+        isBuy={false}
+      />
+    </Box>
   );
 }
 
---
+++ src/Components/ShoppingList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, Box, Typography, List } from "@mui/material";
+import { ShoppingListItem } from "./ShoppingListItem";
+
+const ShoppingList = ({ elements, onToggleBuy, isBuy }) => {
+  return (
+    <div>
+      <Card variant="outlined" sx={{ minWidth: 300, paddingTop: 2 }}>
+        <Box display="flex" flexDirection="column">
+          <Typography variant="subtitle1" align="center" mb={4}>
+            Título
+          </Typography>
+          <List>
+            {elements
+              .filter((elem) => elem.inStock === isBuy)
+              .map((elem) => (
+                <ShoppingListItem
+                  key={`shopping-item.${elem.name}`}
+                  label={elem.name}
+                  isBuy={isBuy}
+                  onToggleBuy={onToggleBuy}
+                />
+              ))}
+          </List>
+        </Box>
+      </Card>
+    </div>
+  );
+};
+
+export { ShoppingList };
