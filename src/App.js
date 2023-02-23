f8b28cba29b54306192cfbd64942fd5389a701da
García Rueda <miguel-angel-garcia-rueda@capgemini.com>
2023-02-23 12:04:40 +0100

    handleToggleBuy feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from 'react';
+import { SearchBar } from './Components/SearchBar';
+import { ShoppingList } from './Components/ShoppingList';
+import { mockList } from './Shared/Assets/mockList';
+import { Box, Typography } from '@mui/material';
 
 function App() {
+  const [items, setItems] = useState(mockList);
+
+  const handleToggleBuy = (label) => {
+    const newItems = items.map((elem) => {
+      if (elem.name === label) {
+        elem.inStock = !elem.inStock;
+      }
+      return elem;
+    });
+    setItems(newItems);
+  };
+
+  /*const handleButtonClick = (v) => {
+    const newItem = { name: v, inStock: false };
+    setItems([...items, newItem]);
+  };*/
+
   return (
-    <>
-      <Typography variant="h3" align="center">
+    <Box display='flex' flexDirection='column' gap={4} alignItems='center'>
+      <Typography variant='h3' align='center'>
         Shopping List
       </Typography>
-    </>
+      <SearchBar />
+      <ShoppingList
+        elements={items}
+        onToggleBuy={handleToggleBuy}
+        isBuy={true}
+      />
+      <ShoppingList
+        elements={items}
+        onToggleBuy={handleToggleBuy}
+        isBuy={false}
+      />
+    </Box>
   );
 }
 
---
+++ src/Shared/Assets/mockList.js
@@ -0,0 +1,9 @@
+const mockList = [
+  { name: 'frutas', inStock: false },
+  { name: 'leche', inStock: false },
+  { name: 'cereales', inStock: false },
+  { name: 'cervezas', inStock: true },
+  { name: 'pizza', inStock: true },
+  { name: 'fernet', inStock: true },
+];
+export { mockList };
