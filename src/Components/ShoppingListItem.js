d2765f48f74ed5d582e7fba68bbbbc0834273246
Martínez Díaz <alba.martinez-diaz@capgemini.com>
2023-02-23 12:04:46 +0100

    HandlerToggleBuy feature

---
+++ src/Components/ShoppingListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ListItem, ListItemText, IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import CheckIcon from "@mui/icons-material/Check";
+
+const ShoppingListItem = ({ label, isBuy, onToggleBuy }) => {
+  return (
+    <ListItem
+      secondaryAction={
+        <IconButton
+          aria-label="action-button"
+          onClick={() => onToggleBuy(label)}
+        >
+                    {isBuy ? <CheckIcon /> : <AddIcon />}       {" "}
+        </IconButton>
+      }
+    >
+      <ListItemText id={label} primary={label} />
+    </ListItem>
+  );
+};
+
+export { ShoppingListItem };
